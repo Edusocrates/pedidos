33913377063761a28a231e4c3a1355f3011cb87c
edusocrates <edu5390@gmail.com>
2023-08-23 16:11:21 -0300

    Initial commit

---
+++ src/main/java/br/com/estudos/edusocrates/pedidos/dto/PedidoDto.java
@@ -0,0 +1,23 @@
+package br.com.estudos.edusocrates.pedidos.dto;
+
+import br.com.estudos.edusocrates.pedidos.model.Status;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PedidoDto {
+
+    private Long id;
+    private LocalDateTime dataHora;
+    private Status status;
+    private List<ItemDoPedidoDto> itens = new ArrayList<>();
+
+
+
+}
---
+++ src/main/java/br/com/estudos/edusocrates/pedidos/dto/StatusDto.java
@@ -0,0 +1,15 @@
+package br.com.estudos.edusocrates.pedidos.dto;
+
+import br.com.estudos.edusocrates.pedidos.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusDto {
+    private Status status;
+}
