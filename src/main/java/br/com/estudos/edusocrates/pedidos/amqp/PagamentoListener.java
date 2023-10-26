957af77346c3c39826d6ce3fc8c13d82cf2ff10b
edusocrates <edu5390@gmail.com>
2023-10-25 22:08:38 -0300

    configurando rabbitListener

---
+++ src/main/java/br/com/estudos/edusocrates/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,16 @@
+package br.com.estudos.edusocrates.pedidos.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+    @RabbitListener(queues = "pagamento.concluido")
+    public void recebeMensagem(Message mensagem){
+        System.out.println("Recebi a mensagem: "+ mensagem.toString());
+    }
+
+
+}
